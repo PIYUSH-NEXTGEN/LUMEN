1d7b27442eb9132d4a4eda85b4e3d66c3cbc8347
Piyush Baraskar <piyushbaraskar4@gmail.com>
2026-08-23 03:05:37 +0530

    feat: enhance Image and DuplicateGroup models with additional attributes and relationships

--- image_analyzer/database/models.py
+++ image_analyzer/database/models.py
@@ -1,8 +1,7 @@
-from datetime import datetime
-
-from sqlalchemy.orm import  DeclarativeBase,Mapped, mapped_column
+from sqlalchemy.orm import  DeclarativeBase,Mapped, mapped_column, relationship
 from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy import String, DateTime
+from sqlalchemy import String, DateTime, ForeignKey, func
+
 
 class Base(DeclarativeBase):
     pass
@@ -12,44 +11,49 @@ class Image(Base):
     __tablename__ = "images"
 
     id: Mapped[ int] = mapped_column(primary_key=True)
-    filename: Mapped[ str] = mapped_column(String(255))
-    file_path: Mapped[ str] = mapped_column(String(255))
-    file_hash: Mapped[ str] = mapped_column(String(255))
-    analyzed_at: Mapped[ str] = mapped_column(String(255))
+    filename: Mapped[ str] = mapped_column(String(255), nullable=False)
 
-    width: Mapped[int]
-    height: Mapped[int]
-    img_dtype: Mapped[str]
-    mean: Mapped[float]
-    std: Mapped[float]
-    minimum: Mapped[int]
-    maximum: Mapped[int]
+    duplicate_group_id: Mapped[int | None] = mapped_column(
+        ForeignKey("duplicate_groups.id",ondelete="SET NULL"),
+        nullable=True )
 
-    mean_brightness: Mapped[float]
-    luminance_brightness: Mapped[float]
-    contrast_score: Mapped[float]
-    sharpness_score: Mapped[float]
-    colorfulness_score: Mapped[float]
-    underexposed_pct: Mapped[float]
-    overexposed_pct: Mapped[float]
-    entropy_score: Mapped[float]
+    duplicate_group: Mapped["DuplicateGroup | None"] = relationship(
+        back_populates="images"
+    )
 
-    channel_stats: Mapped[dict] = mapped_column(JSONB)
-    histogram_regions: Mapped[dict] = mapped_column(JSONB)
-    dominant_colors: Mapped[list] = mapped_column(JSONB)
+    file_path: Mapped[ str] = mapped_column(String(1000), nullable=False)
+    file_hash: Mapped[ str] = mapped_column(String(64),  nullable=False)
+    analyzed_at: Mapped[ DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+    width: Mapped[int] = mapped_column(nullable=True)
+    height: Mapped[int] = mapped_column(nullable=True)
+    img_dtype: Mapped[str | None] = mapped_column(String(50), nullable=True)
+    mean: Mapped[float | None] = mapped_column(nullable=True)
+    std: Mapped[float | None] = mapped_column(nullable=True)
+    minimum: Mapped[int | None] = mapped_column(nullable=True)
+    maximum: Mapped[int | None] = mapped_column(nullable=True)
+
+    mean_brightness: Mapped[float | None] = mapped_column(nullable=True)
+    luminance_brightness: Mapped[float | None] = mapped_column(nullable=True)
+    contrast_score: Mapped[float | None] = mapped_column(nullable=True)
+    sharpness_score: Mapped[float | None] = mapped_column(nullable=True)
+    colorfulness_score: Mapped[float | None] = mapped_column(nullable=True)
+    underexposed_pct: Mapped[float | None] = mapped_column(nullable=True)
+    overexposed_pct: Mapped[float | None] = mapped_column(nullable=True)
+    entropy_score: Mapped[float | None] = mapped_column(nullable=True)
+
+    channel_stats: Mapped[dict] = mapped_column(JSONB,nullable=True)
+    histogram_regions: Mapped[dict] = mapped_column(JSONB,nullable=True)
+    dominant_colors: Mapped[list] = mapped_column(JSONB,nullable=True)
 
 class DuplicateGroup(Base):
     __tablename__ = "duplicate_groups"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    hash: Mapped[str] = mapped_column(String(64), unique=True )
+    created_at: Mapped[DateTime] = mapped_column(DateTime(timezone=True), server_default=func.now(),nullable=False)
 
-    hash: Mapped[str] = mapped_column(
-        String(64),
-        unique=True
-    )
-
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
-
+    images: Mapped[list["Image"]] = relationship(back_populates="duplicate_group",passive_deletes=True)
 
 
 
